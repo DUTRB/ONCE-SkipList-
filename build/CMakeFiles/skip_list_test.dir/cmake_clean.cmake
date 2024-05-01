8b8d41f2826d0ebb12627b6921df345a4648baf1
HUAWEI-Ubuntu <ruluy0205@163.com>
2024-05-01 20:48:55 +0800

    run

---
+++ build/CMakeFiles/skip_list_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/skip_list_test.dir/test/stress_test.cc.o"
+  "CMakeFiles/skip_list_test.dir/test/stress_test.cc.o.d"
+  "skip_list_test"
+  "skip_list_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/skip_list_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
